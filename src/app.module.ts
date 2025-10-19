eaef4ade1b3d5f13d627332cf6b581870b5797ba
Santiago Angel <1193571417@u.icesi.edu.co>
2025-10-19 15:22:32 -0500

    feat:gym project modules created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MembershipsModule } from './memberships/memberships.module';
+import { AuthModule } from './auth/auth.module';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { AttendancesModule } from './attendances/attendances.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, MembershipsModule, AuthModule, SubscriptionsModule, AttendancesModule],
   controllers: [AppController],
   providers: [AppService],
 })
