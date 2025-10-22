6c4b65462b3a02b58020c90943689f211fe03160
Lrojas898 <luis.manuel.rojas71@gmail.comm>
2025-10-22 10:32:27 -0500

    feat: deleted permission reference

--- src/auth/entities/roles.entity.ts
+++ src/auth/entities/roles.entity.ts
@@ -1,6 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, JoinTable } from "typeorm";
 import { User } from "./users.entity";         
-import { Permission } from "./permissions.entity";
 import { ValidRoles } from "../enums/roles.enum";
 
 @Entity('roles')
@@ -17,13 +16,4 @@ export class Role {
     
     @ManyToMany(() => User, user => user.roles)
     users: User[];
-
-    
-    @ManyToMany(() => Permission, { eager: false })
-    @JoinTable({
-        name: 'role_permissions',
-        joinColumn: { name: 'roleId', referencedColumnName: 'id' },
-        inverseJoinColumn: { name: 'permissionId', referencedColumnName: 'id' }
-    })
-    permissions: Permission[];
 }
